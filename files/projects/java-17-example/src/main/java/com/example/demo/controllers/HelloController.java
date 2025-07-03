29a85d57e8ecbcabe5f36c97b64298ed44381f4f
Waltenne Carvalho <waltenne.ferreira@hotmail.com>
2025-07-03 10:49:25 -0300

    fix: adjust java project

--- files/projects/java-17-example/src/main/java/com/example/demo/controllers/HelloController.java
+++ files/projects/java-17-example/src/main/java/com/example/demo/controllers/HelloController.java
@@ -16,7 +16,7 @@ public class HelloController extends HttpServlet {
     }
     
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String name = req.getParameter("name");
         name = determineName(name); // Reuse o método
         
